1031f537dec92e1078adcd208aac4d2c49dd6337
MaheshKharat297 <142563060+MaheshKharat297@users.noreply.github.com>
2024-09-03 19:32:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from core.config import settings
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/test")
+def hello_api():
+    return {"message": "hello api"}
